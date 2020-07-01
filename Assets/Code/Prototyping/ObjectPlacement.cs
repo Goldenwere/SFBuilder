2a0361a4c2ff071b84aaaa3c56a1bf58ef971ebf
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-06-30 21:09:49 -0400

    place cube using ScreenPointToRay
    idea taken from DevConsole object selection in other project UE2

---
+++ Assets/Code/Prototyping/ObjectPlacement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ObjectPlacement : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField] private GameObject prefab;
+#pragma warning restore
+    /**************/ private GameObject prefabInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefabInstance = Instantiate(prefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out RaycastHit hit, 1000f))
+            prefabInstance.transform.position = hit.point;
+
+        if (Mouse.current.leftButton.ReadValue() > 0)
+            prefabInstance = Instantiate(prefab);
+    }
+}
