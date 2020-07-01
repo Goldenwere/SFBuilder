530a4885742afbb09d188f5aa0d20e3c2f1843d6
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-07-01 17:50:01 -0400

    create gamescoring mechanism

---
+++ Assets/Code/Prototyping/GameScoring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScoring : MonoBehaviour
+{
+    public GameScoring Instance { get; private set; }
+    public int Score { get; set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), string.Format("Score: {0}", Score), new GUIStyle { normal = new GUIStyleState { textColor = Color.magenta }, fontSize = 32 });
+    }
+}
