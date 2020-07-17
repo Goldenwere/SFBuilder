ccfb55df2d4e24dac5e1adf1505e78b6e922f2e1
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-07-17 16:49:27 -0400

    began work on level transitions

---
+++ Assets/Code/GameConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace SFBuilder
+{
+    public struct GameConstants
+    {
+        /// <summary>
+        /// Wait period for animating post-level-load transition
+        /// </summary>
+        public const float LevelTransitionEndTime = 2.5f;
+
+        /// <summary>
+        /// Wait period for animating pre-level-load transition
+        /// </summary>
+        public const float LevelTransitionStartTime = 2.5f;
+    }
+}
