d5a11e7f38be940089a0eae469811a4f66fcff3e
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-07-19 15:39:42 -0400

    began work on tooltips
    also fixed missing asmdef for controller

--- Assets/Code/Goldenwere/CoreAPI.cs
+++ Assets/Code/Goldenwere/CoreAPI.cs
@@ -78,6 +78,34 @@ namespace Goldenwere.Unity
         }
 
         /// <summary>
+        /// Find the child with a specific tag of a gameobject
+        /// </summary>
+        /// <param name="parent">The parent gameobject to search from</param>
+        /// <param name="tag">The tag that the child must match</param>
+        /// <returns>The found GameObject or null</returns>
+        public static GameObject FindChildWithTagRecursively(this GameObject parent, string tag)
+        {
+            if (parent == null)
+                throw new System.ArgumentNullException();
+
+            for (int i = 0; i < parent.transform.childCount; i++)
+            {
+                GameObject child = parent.transform.GetChild(i).gameObject;
+                if (child.tag == tag)
+                    return child;
+
+                else if (child.transform.childCount > 0)
+                {
+                    GameObject foundTest = child.FindChildWithTagRecursively(tag);
+                    if (foundTest != null)
+                        return foundTest;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
         /// Clamps a quaternion's vertical rotation
         /// </summary>
         /// <param name="parent">The quaternion to clamp</param>
---
+++ Assets/Code/Goldenwere/UI/TooltipEnabledElement.cs
@@ -0,0 +1,53 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Goldenwere.Unity.UI
+{
+    public class TooltipEnabledElement : MonoBehaviour
+    {
+        [Tooltip         ("Prefab which the topmost gameobject can be resized based on text and contains a text element that can be set")]
+        [SerializeField] private GameObject tooltipPrefab;
+        [Tooltip         ("The text to display in the tooltip")]
+        [SerializeField] private string     tooltipText;
+        [Tooltip         ("Values used if defining a string that needs formatting. Leave blank if no formatting is done inside tooltipText")]
+        [SerializeField] private double[]   tooltipValues;
+        /**************/ private GameObject tooltipSpawnedElement;
+        /**************/ private TMP_Text   tooltipTextElement;
+
+        /// <summary>
+        /// Sets up the tooltip at start
+        /// </summary>
+        private void Start()
+        {
+            tooltipSpawnedElement = Instantiate(tooltipPrefab, transform);
+            tooltipTextElement = tooltipSpawnedElement.GetComponentInChildren<TMP_Text>();
+            SetText();
+        }
+
+        /// <summary>
+        /// Updates the tooltip text with a new tooltip and optional new values (required if the tooltip has formated values)
+        /// </summary>
+        /// <param name="newTooltip">The new tooltip string value</param>
+        /// <param name="newValues">New values to display in the tooltip if the string requires formatting</param>
+        public void UpdateText(string newTooltip, double[] newValues = null)
+        {
+            tooltipText = newTooltip;
+            tooltipValues = newValues;
+            SetText();
+        }
+
+        /// <summary>
+        /// Sets the text element text to the stored tooltipText and resizes the container
+        /// </summary>
+        private void SetText()
+        {
+            if (tooltipValues != null && tooltipValues.Length > 0)
+                tooltipTextElement.text = string.Format(tooltipText, tooltipValues);
+            else
+                tooltipTextElement.text = tooltipText;
+
+            tooltipSpawnedElement.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 
+                tooltipTextElement.renderedHeight + tooltipTextElement.rectTransform.anchoredPosition.y);
+        }
+    }
+}
