88331c436a2181c4dd61391d210b4c5c4c4e5c61
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-08-24 15:14:40 -0400

    testing new method of grounding

--- Assets/Code/Obj/BuilderObjectGrounder.cs
+++ Assets/Code/Obj/BuilderObjectGrounder.cs
@@ -8,11 +8,23 @@ namespace SFBuilder.Obj
     /// </summary>
     public class BuilderObjectGrounder : MonoBehaviour
     {
+        /// <summary>
+        /// Utility class for keeping track of the four corners of the grounder
+        /// </summary>
+        protected class GrounderFace
+        {
+            public Vector3 CornerA;
+            public Vector3 CornerB;
+            public Vector3 CornerC;
+            public Vector3 CornerD;
+        }
+
         #region Fields
 #pragma warning disable 0649
         [SerializeField] private BoxCollider    attachedCollider;
 #pragma warning restore 0649
         /**************/ private List<Collider> collidedObjects;
+        /**************/ private GrounderFace   currentFacePosition;
         /**************/ private const float    rayLength = 0.25f;
         #endregion
         #region Methods
@@ -27,15 +39,38 @@ namespace SFBuilder.Obj
         private void Start()
         {
             collidedObjects = new List<Collider>(16);
+            currentFacePosition = new GrounderFace();
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(currentFacePosition.CornerA, 0.5f);
+            Gizmos.DrawSphere(currentFacePosition.CornerB, 0.5f);
+            Gizmos.DrawSphere(currentFacePosition.CornerC, 0.5f);
+            Gizmos.DrawSphere(currentFacePosition.CornerD, 0.5f);
         }
 
         /// <summary>
-        /// Check if grounded each frame
+        /// Update the current face position each frame
         /// </summary>
         private void Update()
         {
-            IsGrounded = Physics.Raycast(new Ray(attachedCollider.bounds.min, Vector3.down), rayLength) &&
-                Physics.Raycast(new Ray(new Vector3(attachedCollider.bounds.max.x, attachedCollider.bounds.max.y - attachedCollider.size.y, attachedCollider.bounds.max.z), Vector3.down), rayLength);
+            currentFacePosition.CornerA = attachedCollider.bounds.center + new Vector3(attachedCollider.size.x, -attachedCollider.size.y, attachedCollider.size.z) * 0.5f;
+            currentFacePosition.CornerB = attachedCollider.bounds.center + new Vector3(-attachedCollider.size.x, -attachedCollider.size.y, attachedCollider.size.z) * 0.5f;
+            currentFacePosition.CornerC = attachedCollider.bounds.center + new Vector3(-attachedCollider.size.x, -attachedCollider.size.y, -attachedCollider.size.z) * 0.5f;
+            currentFacePosition.CornerD = attachedCollider.bounds.center + new Vector3(attachedCollider.size.x, -attachedCollider.size.y, -attachedCollider.size.z) * 0.5f;
+        }
+
+        /// <summary>
+        /// Check if grounded each physics frame
+        /// </summary>
+        private void FixedUpdate()
+        {
+            IsGrounded = 
+                Physics.Raycast(new Ray(currentFacePosition.CornerA, Vector3.down), rayLength) &&
+                Physics.Raycast(new Ray(currentFacePosition.CornerB, Vector3.down), rayLength) &&
+                Physics.Raycast(new Ray(currentFacePosition.CornerC, Vector3.down), rayLength) &&
+                Physics.Raycast(new Ray(currentFacePosition.CornerD, Vector3.down), rayLength);
         }
 
         /// <summary>
