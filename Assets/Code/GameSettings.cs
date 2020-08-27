bd0e38dd8d4eb642c2e0ff980e6dcf4e805725e6
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-08-27 17:56:19 -0400

    add new settings definitions/defaults/structs for controls

--- Assets/Code/GameSettings.cs
+++ Assets/Code/GameSettings.cs
@@ -13,6 +13,7 @@ namespace SFBuilder
         #region Fields
         private SettingsData    settings;
         #endregion
+
         #region Properties
         /// <summary>
         /// Singleton instance of GameSettings in the base scene
@@ -33,6 +34,7 @@ namespace SFBuilder
             }
         }
         #endregion
+
         #region Methods
         /// <summary>
         /// Set singleton instance on Awake
@@ -80,6 +82,56 @@ namespace SFBuilder
             {
                 settings = new SettingsData
                 {
+                    controlBindings_Gamepad = new ControlBinding_Generic[]
+                    {
+                        new ControlBinding_Generic { control = GenericControl.Camera_MoveBackward,      path = "<Gamepad>/leftStick/down" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_MoveForward,       path = "<Gamepad>/leftStick/up" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_MoveLeft,          path = "<Gamepad>/leftStick/left" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_MoveRight,         path = "<Gamepad>/leftStick/right" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_RotateLeft,        path = "<Gamepad>/leftTrigger" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_RotateRight,       path = "<Gamepad>/rightTrigger" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_TiltDown,          path = "<Gamepad>/rightShoulder" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_TiltUp,            path = "<Gamepad>/leftShoulder" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_ZoomIn,            path = "<Gamepad>/leftStick/up" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_ZoomOut,           path = "<Gamepad>/leftStick/down" },
+                        new ControlBinding_Generic { control = GenericControl.Gameplay_CancelAndMenu,   path = "<Gamepad>/start" },
+                        new ControlBinding_Generic { control = GenericControl.Gameplay_Placement,       path = "<Gamepad>/buttonSouth" },
+                        new ControlBinding_Generic { control = GenericControl.Gameplay_Undo,            path = "<Gamepad>/buttonWest" },
+                    },
+                    controlBindings_Keyboard = new ControlBinding_Generic[]
+                    {
+                        new ControlBinding_Generic { control = GenericControl.Camera_MoveBackward,      path = "<Keyboard>/s" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_MoveForward,       path = "<Keyboard>/w" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_MoveLeft,          path = "<Keyboard>/a" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_MoveRight,         path = "<Keyboard>/d" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_RotateLeft,        path = "<Keyboard>/q" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_RotateRight,       path = "<Keyboard>/e" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_TiltDown,          path = "<Keyboard>/f" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_TiltUp,            path = "<Keyboard>/r" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_ZoomIn,            path = "<Keyboard>/g" },
+                        new ControlBinding_Generic { control = GenericControl.Camera_ZoomOut,           path = "<Keyboard>/t" },
+                        new ControlBinding_Generic { control = GenericControl.Gameplay_CancelAndMenu,   path = "<Keyboard>/escape" },
+                        new ControlBinding_Generic { control = GenericControl.Gameplay_Placement,       path = "<Keyboard>/enter" },
+                        new ControlBinding_Generic { control = GenericControl.Gameplay_Undo,            path = "<Keyboard>/backspace" },
+                    },
+                    controlBindings_Other = new ControlBinding_Other[]
+                    {
+                        new ControlBinding_Other { control = OtherControl.Gamepad_CursorDown,           path = "<Gamepad>/rightStick/down" },
+                        new ControlBinding_Other { control = OtherControl.Gamepad_CursorLeft,           path = "<Gamepad>/rightStick/left" },
+                        new ControlBinding_Other { control = OtherControl.Gamepad_CursorRight,          path = "<Gamepad>/rightStick/right" },
+                        new ControlBinding_Other { control = OtherControl.Gamepad_CursorUp,             path = "<Gamepad>/rightStick/up" },
+                        new ControlBinding_Other { control = OtherControl.Gamepad_ZoomToggle,           path = "<Gamepad>/leftStickPress" },
+                        new ControlBinding_Other { control = OtherControl.Mouse_ToggleMovement,         path = "<Mouse>/leftButton" },
+                        new ControlBinding_Other { control = OtherControl.Mouse_ToggleRotation,         path = "<Mouse>/rightButton" },
+                        new ControlBinding_Other { control = OtherControl.Mouse_ToggleZoom,             path = "<Keyboard>/ctrl" },
+                    },
+                    controlSetting_HoldModifiers = false,
+                    controlSetting_InvertHorizontal = false,
+                    controlSetting_InvertScroll = false,
+                    controlSetting_InvertVertical = false,
+                    controlSetting_SensitivityMovement = 1,
+                    controlSetting_SensitivityRotation = 1,
+                    controlSetting_SensitivityZoom = 1,
                     postprocAO = false,
                     postprocBloom = false,
                     postprocSSR = false,
@@ -129,10 +181,41 @@ namespace SFBuilder
     /// </summary>
     public struct SettingsData
     {
-        public bool     postprocAO;
-        public bool     postprocBloom;
-        public bool     postprocSSR;
-        public float    volEffects;
-        public float    volMusic;
+        public ControlBinding_Generic[] controlBindings_Gamepad;
+        public ControlBinding_Generic[] controlBindings_Keyboard;
+        public ControlBinding_Other[]   controlBindings_Other;
+
+        public bool                     controlSetting_HoldModifiers;
+        public bool                     controlSetting_InvertHorizontal;
+        public bool                     controlSetting_InvertScroll;
+        public bool                     controlSetting_InvertVertical;
+        public float                    controlSetting_SensitivityMovement;
+        public float                    controlSetting_SensitivityRotation;
+        public float                    controlSetting_SensitivityZoom;
+
+        public bool                     postprocAO;
+        public bool                     postprocBloom;
+        public bool                     postprocSSR;
+
+        public float                    volEffects;
+        public float                    volMusic;
+    }
+
+    /// <summary>
+    /// Data structure for associating GenericControl to a path
+    /// </summary>
+    public struct ControlBinding_Generic
+    {
+        public GenericControl   control;
+        public string           path;
+    }
+
+    /// <summary>
+    /// Data structure for associating OtherControl to a path
+    /// </summary>
+    public struct ControlBinding_Other
+    {
+        public OtherControl     control;
+        public string           path;
     }
 }
