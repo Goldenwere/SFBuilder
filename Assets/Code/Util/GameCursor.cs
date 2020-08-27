bf98d36476a6009e05e6592a2866db5057e7ab36
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-08-27 15:25:41 -0400

    implement custom cursor mechanism
    this bypasses the 32x32 size limitation of the normal cursor

---
+++ Assets/Code/Util/GameCursor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using Goldenwere.Unity.Controller;
+
+namespace SFBuilder.Util
+{
+    /// <summary>
+    /// Handles the UI and positioning of the game cursor
+    /// </summary>
+    public class GameCursor : MonoBehaviour
+    {
+        /**************/ public  Vector2    cursorSize;
+#pragma warning disable 0649
+        [SerializeField] private Sprite     cursor;
+#pragma warning restore 0649
+
+        private void Start()
+        {
+            Cursor.visible = false;
+        }
+
+        private void OnGUI()
+        {
+            Vector2 pos = Mouse.current.position.ReadValue();
+            pos.y = Screen.height - pos.y;
+            GUI.DrawTexture(new Rect(pos, cursorSize), cursor.texture);
+        }
+    }
+}
