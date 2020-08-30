67fcc54917589e22c2e10259ec5c0bdbcb618ade
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-08-30 12:17:52 -0400

    extend unity buttons for controls

--- Assets/Code/Control.cs
+++ Assets/Code/Control.cs
@@ -1,6 +1,50 @@
 ﻿namespace SFBuilder
 {
     /// <summary>
+    /// Definitions of controls for game input
+    /// </summary>
+    public enum GameControl : byte
+    {
+        Camera_MoveBackward = 0,
+        Camera_MoveForward = 1,
+        Camera_MoveLeft = 2,
+        Camera_MoveRight = 3,
+
+        Camera_RotateLeft = 4,
+        Camera_RotateRight = 5,
+        Camera_TiltUp = 6,
+        Camera_TiltDown = 7,
+
+        Camera_ZoomIn = 8,
+        Camera_ZoomOut = 9,
+
+        Gameplay_CancelAndMenu = 10,
+        Gameplay_Placement = 11,
+        Gameplay_Undo = 12,
+
+        Gamepad_CursorDown = 13,
+        Gamepad_CursorLeft = 14,
+        Gamepad_CursorRight = 15,
+        Gamepad_CursorUp = 16,
+
+        Gamepad_ZoomToggle = 17,
+
+        Mouse_ToggleMovement = 18,
+        Mouse_ToggleRotation = 19,
+        Mouse_ToggleZoom = 20
+    }
+
+    /// <summary>
+    /// Definitions of supported game input types
+    /// </summary>
+    public enum InputType : byte
+    {
+        Keyboard = 0,
+        Mouse = 1,
+        Gamepad = 2
+    }
+
+    /// <summary>
     /// Definitions of generic controls, i.e., those that are used for multiple sets of bindings (one for keyboard, one for gamepad)
     /// </summary>
     public enum GenericControl : byte
---
+++ Assets/Code/UI/ControlButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SFBuilder.UI
+{
+    /// <summary>
+    /// Extends the UnityEngine.UI.Button to allow for associating controls-related information to buttons
+    /// </summary>
+    public class ControlButton : Button
+    {
+        [Header("Controls")]
+#pragma warning disable 0649
+        [SerializeField] private GameControl    associatedControl;
+        [SerializeField] private InputType[]    expectedInput;
+#pragma warning restore 0649
+
+        /// <summary>
+        /// The control associated with the button
+        /// </summary>
+        public GameControl  AssociatedControl   { get { return associatedControl; } }
+
+        /// <summary>
+        /// The expected inputs for the button
+        /// </summary>
+        public InputType[]  ExpectedInput       { get { return expectedInput; } }
+    }
+}
