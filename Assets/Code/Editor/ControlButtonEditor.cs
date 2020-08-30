5d82e2cc60d705587ac831b9dbfe239e266137e0
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-08-30 14:02:47 -0400

    create editor script for ControlButton and attach ControlButton to controls menu buttons

---
+++ Assets/Code/Editor/ControlButtonEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+
+namespace SFBuilder.UI
+{
+    /// <summary>
+    /// Editor code for the ControlButton
+    /// </summary>
+    [CustomEditor(typeof(ControlButton))]
+    public class ControlButtonEditor : UnityEditor.UI.ButtonEditor
+    {
+        /// <summary>
+        /// Overrides and calls OnInspectorGUI for Button before displaying additional fields created by the ControlButton
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            EditorGUILayout.Separator();
+            SerializedProperty control = serializedObject.FindProperty("associatedControl");
+            EditorGUILayout.PropertyField(control);
+            SerializedProperty input = serializedObject.FindProperty("expectedInput");
+            EditorGUILayout.PropertyField(input);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
