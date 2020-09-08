288bf3927ad16a4e68f5f136cd867f9d575a9acd
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-09-08 12:51:41 -0400

    Update GameCursor.cs
    fix dividebyzero exception

--- Assets/Code/Util/GameCursor.cs
+++ Assets/Code/Util/GameCursor.cs
@@ -111,8 +111,8 @@ namespace SFBuilder.Util
             else
             {
                 hardwareCursor = false;
-                cursorSize = new Vector2(Screen.width / (((int)CursorSize.VeryLarge - (int)GameSettings.Instance.Settings.display_Cursor) * 16),
-                    Screen.width / (((int)CursorSize.VeryLarge - (int)GameSettings.Instance.Settings.display_Cursor) * 16));
+                cursorSize = new Vector2(Screen.width / (((int)CursorSize.VeryLarge + 1 - (int)GameSettings.Instance.Settings.display_Cursor) * 16),
+                    Screen.width / (((int)CursorSize.VeryLarge + 1 - (int)GameSettings.Instance.Settings.display_Cursor) * 16));
             }
         }
         #endregion
