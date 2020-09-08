db5becfbb4949ae37c19bc825edb729aeaa3ffbe
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-09-08 18:09:32 -0400

    create ScaleableUI.cs
    used for applying UI scaling at runtime
    
    currently, this is only for HUD icons; the fontsize setting may get expanded to cover UI scaling in general

---
+++ Assets/Code/UI/ScaleableUI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SFBuilder.UI
+{
+    /// <summary>
+    /// Functions similarly to StyleableText, in that it applies updates to settings related to UI scaling
+    /// </summary>
+    [RequireComponent(typeof(Graphic))]
+    public class ScaleableUI : MonoBehaviour
+    {
+        /**************/ private Graphic    graphic;
+
+        /// <summary>
+        /// Initialize text on Start
+        /// </summary>
+        private void Start()
+        {
+            graphic = GetComponent<Graphic>();
+            OnSettingsUpdated();
+        }
+
+        /// <summary>
+        /// Subscribe to events OnEnable
+        /// </summary>
+        private void OnEnable()
+        {
+            GameEventSystem.SettingsUpdated += OnSettingsUpdated;
+        }
+
+        /// <summary>
+        /// Unsubscribe from events OnDisable
+        /// </summary>
+        private void OnDisable()
+        {
+            GameEventSystem.SettingsUpdated -= OnSettingsUpdated;
+        }
+
+        /// <summary>
+        /// Handle GameEventSystem.SettingsUpdated by applying font settings
+        /// </summary>
+        private void OnSettingsUpdated()
+        {
+            switch(GameSettings.Instance.Settings.accessibility_FontSize)
+            {
+                case FontSize.Large:  graphic.rectTransform.localScale = new Vector3(1.25f, 1.25f, 1.25f); break;
+                case FontSize.Small:  graphic.rectTransform.localScale = new Vector3(0.75f, 0.75f, 0.75f); break;
+                case FontSize.Medium:
+                default:              graphic.rectTransform.localScale = new Vector3(1.00f, 1.00f, 1.00f); break;
+            }
+        }
+    }
+}
