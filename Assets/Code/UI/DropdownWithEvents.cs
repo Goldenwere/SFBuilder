cf92dbec9d12e917a8b0092ec1fa270e4bd9bad4
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-11-24 13:17:14 -0500

    document dropdownwithevents

--- Assets/Code/UI/DropdownWithEvents.cs
+++ Assets/Code/UI/DropdownWithEvents.cs
@@ -4,8 +4,17 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// Override dropdowns in order to make them keyboard navigable
+/// </summary>
+/// <remarks>Dropdowns are normally navigable, but in making other things navigable, it breaks this</remarks>
 public class DropdownWithEvents : TMP_Dropdown
 {
+    /// <summary>
+    /// Override for CreateDropdownList in order to select the itme
+    /// </summary>
+    /// <param name="template">The template to create the list from</param>
+    /// <returns>The created dropdown list</returns>
     protected override GameObject CreateDropdownList(GameObject template)
     {
         GameObject created = base.CreateDropdownList(template);
@@ -13,6 +22,9 @@ public class DropdownWithEvents : TMP_Dropdown
         return created;
     }
 
+    /// <summary>
+    /// Selects the first item in the list after waiting until end of frame (so that the list is properly instantiated)
+    /// </summary>
     private IEnumerator WaitBeforeSelection()
     {
         yield return new WaitForEndOfFrame();
