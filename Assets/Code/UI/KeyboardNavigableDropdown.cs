e1ca22abc34a9f931da3cce0283db2b4b8ae3c07
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-11-24 14:39:38 -0500

    fix dropdown not selecting itself after submitting option

--- Assets/Code/UI/KeyboardNavigableDropdown.cs
+++ Assets/Code/UI/KeyboardNavigableDropdown.cs
@@ -11,23 +11,37 @@ using UnityEngine.EventSystems;
 public class KeyboardNavigableDropdown : TMP_Dropdown
 {
     /// <summary>
-    /// Override for CreateDropdownList in order to select the itme
+    /// Override for CreateDropdownList in order to select the item
     /// </summary>
     /// <param name="template">The template to create the list from</param>
     /// <returns>The created dropdown list</returns>
     protected override GameObject CreateDropdownList(GameObject template)
     {
         GameObject created = base.CreateDropdownList(template);
-        StartCoroutine(WaitBeforeSelection());
+        StartCoroutine(WaitBeforeSelection(false));
         return created;
+    }
+
+    /// <summary>
+    /// Override for DestroyDropdownList in order to select the dropdown
+    /// </summary>
+    /// <param name="dropdownList">The dropdown list to destroy</param>
+    protected override void DestroyDropdownList(GameObject dropdownList)
+    {
+        base.DestroyDropdownList(dropdownList);
+        StartCoroutine(WaitBeforeSelection(true));
     }
 
     /// <summary>
     /// Selects the first item in the list after waiting until end of frame (so that the list is properly instantiated)
     /// </summary>
-    private IEnumerator WaitBeforeSelection()
+    /// <param name="selectList"></param>
+    private IEnumerator WaitBeforeSelection(bool selectList)
     {
         yield return new WaitForEndOfFrame();
-        EventSystem.current.SetSelectedGameObject(gameObject.FindChild("Dropdown List").FindChildRecursively("Content").transform.GetChild(1).gameObject);
+        if (selectList)
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        else
+            EventSystem.current.SetSelectedGameObject(gameObject.FindChild("Dropdown List").FindChildRecursively("Content").transform.GetChild(1).gameObject);
     }
 }
