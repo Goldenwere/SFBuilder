3c4dd63d10a062c44d470c727dd875de97d297a4
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-11-24 11:56:44 -0500

    First attempt at making dropdowns select options from keyboard

---
+++ Assets/Code/UI/DropdownWithEvents.cs
@@ -0,0 +1,27 @@
+﻿using Goldenwere.Unity;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropdownWithEvents : TMP_Dropdown, IPointerClickHandler, ISubmitHandler
+{
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+        StartCoroutine(WaitForOptionsEnabled());
+    }
+
+    public override void OnSubmit(BaseEventData eventData)
+    {
+        base.OnSubmit(eventData);
+        StartCoroutine(WaitForOptionsEnabled());
+    }
+
+    private IEnumerator WaitForOptionsEnabled()
+    {
+        while (!template.gameObject.activeInHierarchy)
+            yield return new WaitForFixedUpdate();
+        EventSystem.current.SetSelectedGameObject(template.gameObject.FindChildRecursively("Content").transform.GetChild(0).gameObject);
+    }
+}
